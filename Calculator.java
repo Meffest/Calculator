abb4c57b9be210b671ed736df08f2936475276fe
Andzhey Koba <andzheykoba@gmail.com>
2016-05-10 15:30:06 +0300

    Add subtraction, multiplication and division operation.

--- Calculator.java
+++ Calculator.java
@@ -1,35 +1,76 @@
+// import ru.lessons.lesson_8.UserException;
+
 /**
 	Класс реализует калькулятор.
 */
 public class Calculator {
 	/**
-		Рузультат вычисления.
-	*/
+	 Результат вычисления.
+	 */
 	private int result;
 
 	/**
-		Суммируем аргументы.
-	    @param params Аргументы суммирования.
-	*/
-	public void add(int ... params){
-		for(Integer param : params){
-			this.result +=param;
+	 * Суммируем аргументы.
+	 * @param params Аргументы суммирования.
+	 */
+	public void add(int ... params) {
+		for (int param : params) {
+			this.result += param;
 		}
 	}
 
 	/**
-		Получить результат.
-		@return результат вычисления
-	*/
-	public int getResult(){
+	 * Вычитаем аргументы.
+	 * @param params Аргументы разности.
+	 */
+	public void sub(int ... params) {
+		this.result = params[0] * 2;
+		for (int param : params) {
+			this.result -= param;
+		}
+	}
+
+	/**
+	 * Перемножаем аргументы.
+	 * @param params Аргументы произведения.
+	 */
+	public void mult(int ... params) {
+		for (int param : params) {
+			this.result *= param;
+		}
+	}
+
+	/**
+	 * Вычиляем деление.
+	 * @param args входящие агрументы.
+	 * @throws ru.lessons.lesson_8.UserException Если агрументов нет, выкидывает исключение.
+	 */
+	public void div(int ... args) /**throws UserException*/ {
+		// if (args.length > 0) {
+			this.result = args[0];
+			for (int params : args) {
+				// if (params == 0) {
+				// 	throw new IllegalArgumentException("You try to div by 0. Please change arg!");
+				// }
+				this.result /= params;
+			}
+		// } else {
+		// 	throw new UserException("You should enter args!");
+		// }
+	}
+
+	/**
+	 * Получить результат.
+	 * @return результат вычисления.
+	 */
+	public int getResult() {
 		return this.result;
 	}
 
 	/**
-		Очистить результат вычисления.
-	*/
-	public void cleanResult(){
+	 * Очистить результат вычисления.
+	 */
+	public void cleanResult() {
 		this.result = 0;
 	}
-
 }
