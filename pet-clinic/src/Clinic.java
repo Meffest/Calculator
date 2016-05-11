10c1998281232d193c31d106490229a9dc628258
Andzhey Koba <andzheykoba@gmail.com>
2016-05-11 22:57:43 +0300

    Create class Clinic

--- pet-clinic/src/Clinic.java
+++ pet-clinic/src/Clinic.java
@@ -1,5 +1,12 @@
+import java.util.List;
+
 /**
  * Created by Анджей on 11.05.2016.
  */
 public class Clinic {
+    private final List<Client> clients;
+
+    public Clinic(List<Client> clients) {
+        this.clients = clients;
+    }
 }
