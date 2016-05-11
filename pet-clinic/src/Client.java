3a5f89ac1176ad20b5ab541bd487765a6f7d0e3f
Andzhey Koba <andzheykoba@gmail.com>
2016-05-11 22:57:03 +0300

    Create class client

--- pet-clinic/src/Client.java
+++ pet-clinic/src/Client.java
@@ -2,4 +2,11 @@
  * Created by Анджей on 11.05.2016.
  */
 public class Client {
+    private final Pet pet;
+    private final String id;
+
+    public Client(Pet pet, String id) {
+        this.pet = pet;
+        this.id = id;
+    }
 }
