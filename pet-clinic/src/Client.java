9695b8dde68d26fefb7ae531f2fa7e139dca98af
Andzhey Koba <andzheykoba@gmail.com>
2016-05-13 17:33:26 +0300

    Create field name type String? and add getter and setter

--- pet-clinic/src/Client.java
+++ pet-clinic/src/Client.java
@@ -2,11 +2,37 @@
  * Created by Анджей on 11.05.2016.
  */
 public class Client {
-    private final Pet pet;
-    private final String id;
+    private int id;
+    private String name;
+    private Pet pet;
 
-    public Client(Pet pet, String id) {
-        this.pet = pet;
+    public Client(int id, String name, Pet pet) {
         this.id = id;
+        this.name = name;
+        this.pet = pet;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
     }
 }
