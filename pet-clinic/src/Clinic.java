72083bd0385dde1a86b7491af621b2b5349aa8e3
Andzhey Koba <andzheykoba@gmail.com>
2016-05-13 17:34:33 +0300

    Create metods add client and find client by name

--- pet-clinic/src/Clinic.java
+++ pet-clinic/src/Clinic.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -5,8 +7,40 @@ import java.util.List;
  */
 public class Clinic {
     private final List<Client> clients;
+    int id = 1;
 
-    public Clinic(List<Client> clients) {
-        this.clients = clients;
+    public Clinic() {
+        clients = new ArrayList<Client>();
+    }
+
+    /**
+     * Added client
+     * @param client
+     */
+    public void addClient(final Client client) {
+        this.clients.add(client);
+    }
+
+    /**
+     * Find client by name
+     * @throws IOException
+     */
+    public void findClientByName() throws IOException {
+        String clientName;
+        String ask;
+        ConsoleHelper.writeMessage("Введите имя клиента: ");
+        clientName = ConsoleHelper.readString();
+        for (Client client : clients) {
+            if (client.getName().equalsIgnoreCase(clientName)) {
+                ConsoleHelper.writeMessage("Найден клиент: " + client.getId() + " " + client.getName() +
+                        " " + client.getPet().getPetType() + " " + client.getPet().getName());
+                return;
+            }
+        }
+        ConsoleHelper.writeMessage("Клиент не найден. Хотите повторить запрос? yes/no");
+        ask = ConsoleHelper.readString();
+        if (ask.equalsIgnoreCase("yes")) {
+            findClientByName();
+        }else return;
     }
 }
