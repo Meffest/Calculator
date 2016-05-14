65f1e715889b69f60c2a11f77aca2e56508f424b
Andzhey Koba <andzheykoba@gmail.com>
2016-05-14 16:07:21 +0300

    Override metods Pet in ChudoUdo

--- pet-clinic/src/ChudoUdo.java
+++ pet-clinic/src/ChudoUdo.java
@@ -1,5 +1,31 @@
 /**
  * Created by Анджей on 14.05.2016.
  */
-public class ChudoUdo {
+public class ChudoUdo implements Pet {
+    private String name;
+    private String petType;
+
+    public void setPetType(String petType) {
+        this.petType = petType;
+    }
+
+    public ChudoUdo(final String name, final String petType) {
+        this.name = name;
+        this.petType = petType;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public  String getPetType() {
+        return this.petType;
+    }
 }
